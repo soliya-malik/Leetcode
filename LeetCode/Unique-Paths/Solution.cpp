5b0b922c43f9a3ab52076718a7f3cce75f7e9a40
soliya-malik <150206622+soliya-malik@users.noreply.github.com>
2026-08-16 11:11:30 +0530

    Add solution for Unique Paths

---
+++ LeetCode/Unique-Paths/Solution.cpp
@@ -0,0 +1,16 @@
+1class Solution {
+2public:
+3    int uniquePaths(int m, int n) {
+4        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+5        dp[m-1][n-1]=1;
+6
+7        for(int i = m-1;i>=0;i--){
+8            for(int j = n-1;j>=0;j--){
+9                 if(i == m-1 && j == n-1)
+10                    continue;
+11                dp[i][j]=dp[i+1][j] +dp[i][j+1];
+12            }
+13        }
+14        return dp[0][0];
+15    }
+16};

6751dd9a4cb125854cd6b5c1bafea2d9b5d3d2ed
soliya-malik <150206622+soliya-malik@users.noreply.github.com>
2026-09-03 11:32:05 +0530

    Add solution for Exchange Seats

--- LeetCode/Exchange-Seats/Solution.sql
+++
@@ -1,8 +0,0 @@
-1# Write your MySQL query statement below
-2select 
-3case
-4when id = (select max(id) from seat) and id%2=1 then id
-5when id%2=1 then id+1 else id-1 end as id,
-6student
-7from seat 
-8order by id
